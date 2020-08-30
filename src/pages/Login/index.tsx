ddfc55dedf7caaeccfd03dea5633c22fc4770e7d
karinetag2u <karinetag2u@tag2u.com.br>
2020-08-30 01:41:56 -0300

    tela de login básica

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, ImageBackground, View, TextInput, Image } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import BgLogin from '../../assets/img/bgLogin.png';
-import LogoQB from '../../assets/img/logoQB.png';
+import bgLogo from '../../assets/img/bgLogo.png';
+
+import styles from './styles';
 
 function Login() {
     return (
-        <Text>Hello Word</Text>
+      <View style={styles.container}>
+        <ImageBackground resizeMode="contain" source={BgLogin} style={styles.bgImage}>
+          <Image style={styles.logo} source={bgLogo} />
+          <Text style={styles.text}>Seja bem-vindo.</Text>
+            <TextInput style={styles.input} placeholder="Email"/>
+            <TextInput style={styles.input} placeholder="Senha"/>
+          <RectButton style={styles.button}> 
+            <Text style={styles.textButton}>
+              ENTRAR
+            </Text>
+          </RectButton>
+        </ImageBackground>
+      </View>
     );
 }
 
